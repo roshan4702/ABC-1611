afa3deb4029299ffec2a90f2845b63053c912203
roshan4702 <roshan.ece.rymec@gmail.com>
2024-11-16 21:59:19 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -21,7 +21,24 @@ resource "azurerm_virtual_network" "vnet2" {
   location            = var.location
 
   depends_on = [azurerm_resource_group.rg]
+} 
+# Resource: Create Virtual Network Peering
+resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
+  name                       = "vnet1-to-vnet2"
+  resource_group_name        = azurerm_resource_group.rg.name
+  virtual_network_name       = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id  = azurerm_virtual_network.vnet2.id
+  allow_virtual_network_access = true
 }
+
+resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
+  name                       = "vnet2-to-vnet1"
+  resource_group_name        = azurerm_resource_group.rg.name
+  virtual_network_name       = azurerm_virtual_network.vnet2.name
+  remote_virtual_network_id  = azurerm_virtual_network.vnet1.id
+  allow_virtual_network_access = true
+}
+
 
 # Subnet for VM2 in vnet2
 resource "azurerm_subnet" "subnet2" {
